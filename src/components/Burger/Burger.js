3614b078b5eb864f4391bc7019d82c8b4927e707
muticansel <cansel@geradesoftware.com>
2018-07-09 14:29:58 +0300

    deneme

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,8 +4,7 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-	// İş pilgisayarı...
-    let transformedIngredient = Object.keys(props.ingredients)
+	let transformedIngredient = Object.keys(props.ingredients)
         .map(ingredientKey => {
             return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
                 return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />;
