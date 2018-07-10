4f334f0804fbf8e3f33181e98825811882bc03d6
Cansel Muti <cansel@geradesoftware.com>
2018-07-10 11:50:58 +0300

    10.07.18 commit 1

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Auxx';
 
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Aux>
         <Toolbar />
+		<SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <div>LOGO</div>
-        <nav>
-            ...
-        </nav>
-    </header>
+	<header className={classes.Toolbar}>
+		<div>MENU</div>
+		<div className={classes.Logo}>
+			<Logo />
+		</div>
+		<NavigationItems />
+	</header>
 )
 
 export default toolbar;
