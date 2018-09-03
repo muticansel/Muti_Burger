2f2b7c6e8db6521151fd44ffd1e0e85888de58ce
muticansel <cancel.muti@gmail.com>
2018-09-03 21:40:43 +0300

    end of 233

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,12 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Your Name'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
 			},
 			street: {
 				elementType: 'input',
@@ -23,7 +28,12 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Your Street'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
 			},
 			zipCode: {
 				elementType: 'input',
@@ -31,7 +41,14 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'ZIP CODE'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true,
+					minLength: 5,
+					maxLength: 5
+				},
+				valid: false,
+				touched: false
 			},
 			country: {
 				elementType: 'input',
@@ -39,7 +56,12 @@ class ContactData extends Component {
 					type: 'text',
 					placeholder: 'Country'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
 			},
 			email: {
 				elementType: 'input',
@@ -47,7 +69,12 @@ class ContactData extends Component {
 					type: 'email',
 					placeholder: 'Your Email'
 				},
-				value: ''
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
 			},
 			deliveryMethod: {
 				elementType: 'select',
@@ -88,6 +115,24 @@ class ContactData extends Component {
 			});
 	}
 
+	checkValidity(value, rules) {
+		let isValid = true;
+
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid;
+		}
+
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid;
+		}
+
+		if (rules.maxLength) {
+			isValid = value.length <= rules.minLength && isValid;
+		}
+
+		return isValid;
+	}
+
 	inputChangedHandler = (event, inputIdent) => {
 		const updatedOrderForm = {
 			...this.state.orderForm
@@ -98,6 +143,8 @@ class ContactData extends Component {
 		};
 
 		updatedFormElement.value = event.target.value;
+		updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+		updatedFormElement.touched = true;
 		updatedOrderForm[inputIdent] = updatedFormElement;
 		this.setState({ orderForm: updatedOrderForm })
 	}
@@ -119,6 +166,9 @@ class ContactData extends Component {
 						elementType={element.config.elementType}
 						elementConfig={element.config.elementConfig}
 						value={element.config.value}
+						invalid={!element.config.valid}
+						shouldValidate={element.config.validation}
+						touched={element.config.touched}
 						changed={(event) => this.inputChangedHandler(event, element.id)} />
 				))}
 				<Button btnType="Success">ORDER</Button>
